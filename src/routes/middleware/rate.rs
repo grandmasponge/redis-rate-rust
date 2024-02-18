207bff4ac564c0121a1e341905bb7c416ad55606
Toby <73469941+grandmasponge@users.noreply.github.com>
2024-02-18 22:36:27 +0000

    Update rate.rs

--- src/routes/middleware/rate.rs
+++ src/routes/middleware/rate.rs
@@ -110,7 +110,7 @@ where
                     let _: i32 = incr(token.to_string(), arc.clone()).await
                     .expect("Failed to increment");
                     println!("User has made {} requests", val);
-                    if val > self.limit {
+                    if val > 6 {
                         let response = Response::builder()
                             .status(StatusCode::TOO_MANY_REQUESTS)
                             .body(Body::empty())
